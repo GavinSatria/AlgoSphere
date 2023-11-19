38b374dc4ddc92e2077c318eff0e515722c5dfe9
GavinSatria <gavinputrasatria234@gmail.com>
2023-11-19 16:55:38 +0700

    moved inline css to external

--- resources/views/sections/algo_intro.blade.php
+++ resources/views/sections/algo_intro.blade.php
@@ -2,20 +2,18 @@
 @section('content')
 @include('algoprog')
 <div id="main" class="main">
-<h3 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;"><b>What is an algorithm</b></h3>
-    <h5 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;">
+    <h3><b>What is an algorithm</b></h3>
+    <p>
       Algorithm is a step by step set of instructionthat we use to solve a specific problem. 
       the instruction can varies from steps to make a peanut butter sandwich to a guide for our morning daily routine.
       in computer science, algorithm will be used to solve the specific problem that our program are being made for.
-      for example: here is a C program to show </h5>
+      for example: here is a C program to show </p>
       <object data= "{{ asset('/images/introduction to algoprog.jpg') }}" width="700" height="300" style="padding-left: 150px;"></object>
 
     <br></br>
-    <h3 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;"><b>available data types in C language</b></h3>
-    <h5 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;">
-      Algorithm is a step by step procedures to solve problems. it varies from an instruction 
-      to make a peanut butter sandwich to our morning daily routine</h5>
-      
-  </div>
+    <h3><b>available data types in C language</b></h3>
+      <p>Algorithm is a step by step procedures to solve problems. it varies from an instruction 
+      to make a peanut butter sandwich to our morning daily routine</p>
+    
 </div>
 @endsection
--- resources/views/sections/variable.blade.php
+++ resources/views/sections/variable.blade.php
@@ -2,7 +2,10 @@
 @section('content')
 @include('algoprog')
 <div id="main" class="main">
-    <h2>Welcome to the Home Section</h2>
-    <p>This is the default content that appears when the page loads.</p>
+    <h3><b>Variables and Data Types in C Programming Language</b></h3>
+    <p>Variables are named storage locations usually determined by the user to store and manipulate data locally. 
+    <p>Variables have to be declared first. Variable declaration depends on its data type.</p>
+    <p>For example: to declare a variable to store an integer, we use 'int age;'.</p>
+    <p>This creates an integer variable named 'age'.</p>
 </div>
 @endsection
