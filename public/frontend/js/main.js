d739dacec97d56a5bf2175ec9037b5dc33254379
GavinSatria <gavinputrasatria234@gmail.com>
2023-11-19 14:22:24 +0700

    algoprog course page

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -288,6 +288,15 @@ $(document).ready(function(){
         });
     }
 
-
-
 });
+
+function openNav() {
+    document.getElementById("sidenav_algo").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("sidenav_algo").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
