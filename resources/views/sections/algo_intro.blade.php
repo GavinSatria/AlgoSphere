c114cea4f8ce16efd831981e9112a636aee4d3a7
kelv1991 <127710586+kelv1991@users.noreply.github.com>
2023-11-19 16:46:44 +0700

    intro change

--- resources/views/sections/algo_intro.blade.php
+++ resources/views/sections/algo_intro.blade.php
@@ -2,7 +2,20 @@
 @section('content')
 @include('algoprog')
 <div id="main" class="main">
-    <h2>Welcome to the Home Section</h2>
-    <p>This is the default content that appears when the page loads.</p>
+<h3 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;"><b>What is an algorithm</b></h3>
+    <h5 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;">
+      Algorithm is a step by step set of instructionthat we use to solve a specific problem. 
+      the instruction can varies from steps to make a peanut butter sandwich to a guide for our morning daily routine.
+      in computer science, algorithm will be used to solve the specific problem that our program are being made for.
+      for example: here is a C program to show </h5>
+      <object data= "{{ asset('/images/introduction to algoprog.jpg') }}" width="700" height="300" style="padding-left: 150px;"></object>
+
+    <br></br>
+    <h3 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;"><b>available data types in C language</b></h3>
+    <h5 style="color:black; text-align:left; padding-left: 170px; padding-right: 170px;">
+      Algorithm is a step by step procedures to solve problems. it varies from an instruction 
+      to make a peanut butter sandwich to our morning daily routine</h5>
+      
+  </div>
 </div>
 @endsection
