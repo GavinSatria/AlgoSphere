f557eb8999b358ea157e2d9c85bb3bb53949e059
GavinSatria <gavinputrasatria234@gmail.com>
2023-11-20 11:41:45 +0700

    variables

--- resources/views/sections/variable.blade.php
+++ resources/views/sections/variable.blade.php
@@ -3,9 +3,16 @@
 @include('algoprog')
 <div id="main" class="main">
     <h3><b>Variables and Data Types in C Programming Language</b></h3>
+    <br>
     <p>Variables are named storage locations usually determined by the user to store and manipulate data locally. 
-    <p>Variables have to be declared first. Variable declaration depends on its data type.</p>
-    <p>For example: to declare a variable to store an integer, we use 'int age;'.</p>
-    <p>This creates an integer variable named 'age'.</p>
+    <p>Variables have to be declared first. Variable declaration depends on its data type. Commonly used data types include: int (integers) float (decimal numbers), char (characters).</p>
+    <p>For example: 'int age;' declares an integer variable named age.</p>
+    <br>
+    <div style="width: 500px; margin: 0 auto;">
+        <object data="{{ asset('/images/variable table.jpg') }}" width="100%" height="300" style="display: block; margin: 0 auto;"></object>
+        <figcaption style ="text-align: center;">List of different data types.</figcaption>
+    </div>
+    
+    
 </div>
 @endsection
